bc652b8a2348b9188a36be8ae1004a982058e20b
unknown <Administrator@LAP-INLAB039.netsolpk.com>
2022-09-12 12:59:51 +0500

    Add docker file

---
+++ dev/src/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Hello world");
+});
+
+app.get("/status", (request, response) => {
+  const result = {
+    result: "OK - healthy",
+  };
+  response.send(result);
+});
+
+app.get("/metrics", (request, response) => {
+  const result = {
+    data: {
+      UserCount: 140,
+      UserCountActive: 21,
+    },
+  };
+  response.send(result);
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is running at port ${process.env.SERVER_PORT}.`);
+});
